e8461ce82e6a11889437a38e40c14c4bfb4185cc
Jennifer Tang jenntangy@gmail.com <jenntangy@gmail.com>
2017-06-07 17:45:09 +0200

    it works

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,6 +12,7 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
       redirect_to restaurant_path(@restaurant)
